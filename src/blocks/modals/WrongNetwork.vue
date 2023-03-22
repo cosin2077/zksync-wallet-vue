bb0517e9212ac417cd6364eeff83d097bcf18eba
Kevin <conanskyforce@gmail.com>
2023-03-22 16:56:55 +0800

    Update WrongNetwork.vue
    support user switch network, when they met wrong network error

--- src/blocks/modals/WrongNetwork.vue
+++ src/blocks/modals/WrongNetwork.vue
@@ -3,7 +3,16 @@
     <template #header>Wrong network</template>
     <div>
       <div class="_padding-bottom-1">
-        You are on the wrong network. Please switch your wallet to <b>{{ network }}</b> to continue.
+        You are on the wrong network. Please
+          <i-button
+            size="sm"
+            color="primary"
+            :loading="switchLoading"
+            @click="switchNetwork"
+          >
+            switch
+          </i-button>
+        your wallet to <b>{{ network }}</b> to continue.
       </div>
       <div class="_padding-bottom-1 text-sm">
         Some wallets may not support changing networks. If you can not change networks in your wallet you may consider
@@ -14,14 +23,23 @@
 </template>
 
 <script lang="ts">
-import { ZkConfig } from "@matterlabs/zksync-nuxt-core/types";
+import { ZkConfig, ZkEthereumNetwork } from "@matterlabs/zksync-nuxt-core/types";
+import { ethers } from "ethers";
 import Vue from "vue";
 
 export default Vue.extend({
   name: "WrongNetwork",
+  data: () => {
+    return {
+      switchLoading: false,
+    }
+  },
   computed: {
     network(): string {
       return (this.$store.getters["zk-onboard/config"] as ZkConfig).ethereumNetwork.name;
+    },
+    networkInfo(): ZkEthereumNetwork {
+      return (this.$store.getters["zk-onboard/config"] as ZkConfig).ethereumNetwork;
     },
     opened: {
       set(val): void {
@@ -34,6 +52,24 @@ export default Vue.extend({
       },
     },
   },
+  methods: {
+    async switchNetwork() {
+      this.switchLoading = true;
+      try {
+        await window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: ethers.utils.hexValue(this.networkInfo.id) }], // chainId must be in hexadecimal numbers
+        });
+        this.$store.dispatch("zk-onboard/rejectNetworkChange");
+        this.$analytics.track("change_network", { networkName: this.network });
+        this.$store.dispatch("zk-provider/changeNetwork", this.network);
+      } catch (err) {
+        console.log(err)
+      } finally {
+        this.switchLoading = false;
+      }
+    }
+  }
 });
 </script>
 
